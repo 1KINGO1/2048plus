84f2c66a69c05da388fb0efe58ad9519bdcce245
KINGO <spental12345@gmail.com>
2024-06-04 20:02:43 +0300

    Added level configuration for mediaum and hard levels

--- src/main/java/com/example/fx2048plus/config/Config.java
+++ src/main/java/com/example/fx2048plus/config/Config.java
@@ -17,7 +17,7 @@ public class Config {
         {
             add(new Modifier(Modifiers.THREETWOADD, 4));
             add(new Modifier(Modifiers.SHUFFLE, 3));
-            add(new Modifier(Modifiers.LASTCHANCE, 2));
+            add(new Modifier(Modifiers.LASTCHANCE, 1));
             add(new Modifier(Modifiers.X2, 3));
             add(new Modifier(Modifiers.REMOVE, 3));
         }
@@ -29,10 +29,46 @@ public class Config {
     };
     private static final LevelConfig easyLevelConfig = new LevelConfig(5, 64, easyLevelBonuses, easyLevelModifiers);
 
+    private static final List<Modifier> mediumLevelBonuses = new ArrayList<Modifier>() {
+        {
+            add(new Modifier(Modifiers.THREETWOADD, 4));
+            add(new Modifier(Modifiers.SHUFFLE, 3));
+            add(new Modifier(Modifiers.LASTCHANCE, 0));
+            add(new Modifier(Modifiers.X2, 3));
+            add(new Modifier(Modifiers.REMOVE, 2));
+        }
+    };
+    private static final List<Modifier> mediumLevelModifiers = new ArrayList<Modifier>() {
+        {
+            add(new Modifier(Modifiers.TIME, 15));
+        }
+    };
+    private static final LevelConfig mediumLevelConfig = new LevelConfig(4, 2048, mediumLevelBonuses, mediumLevelModifiers);
+
+    private static final List<Modifier> hardLevelBonuses = new ArrayList<Modifier>() {
+        {
+            add(new Modifier(Modifiers.THREETWOADD, 3));
+            add(new Modifier(Modifiers.SHUFFLE, 2));
+            add(new Modifier(Modifiers.LASTCHANCE, 0));
+            add(new Modifier(Modifiers.X2, 1));
+            add(new Modifier(Modifiers.REMOVE, 1));
+        }
+    };
+    private static final List<Modifier> hardLevelModifiers = new ArrayList<Modifier>() {
+        {
+            add(new Modifier(Modifiers.TIME, 15));
+        }
+    };
+    private static final LevelConfig hardLevelConfig = new LevelConfig(4, 4096, hardLevelBonuses, hardLevelModifiers);
+
     public static LevelConfig getLevelConfig(Levels level) {
         switch (level) {
             case EASY:
                 return easyLevelConfig;
+            case MEDIUM:
+                return mediumLevelConfig;
+            case HARD:
+                return hardLevelConfig;
             default:
                 return easyLevelConfig;
         }
