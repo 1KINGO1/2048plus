169c397a6d5f1dfbb4ed194f78348fac8fa672d4
KINGO <spental12345@gmail.com>
2024-06-10 17:53:40 +0300

    Added tail modifier feature

---
+++ src/main/java/com/example/fx2048plus/tile_modifiers/TileModifier.java
@@ -0,0 +1,20 @@
+package com.example.fx2048plus.tile_modifiers;
+
+public interface TileModifier {
+
+    public static double getAppearanceChance() {
+        return 0;
+    }
+
+
+    public boolean isMovable();
+    public boolean isMergeable();
+    // Destroy with bonus
+    public boolean isDestroyable();
+
+    public String getClassName();
+    public void onMerge();
+    public void onDestroy();
+    public void onMove();
+    public void onSpawn();
+}
